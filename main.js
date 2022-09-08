d6f532f60563f94a3fce386beab24aa00b3b03db
Vini35 <viniciusalex0303@gmail.com>
2022-09-08 12:05:00 -0300

    adding light

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import "./style.css";
 
 import * as THREE from "three";
 
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -21,22 +23,37 @@ renderer.setSize(window.innerWidth, innerHeight);
 
 camera.position.setZ(30);
 
-const geometry = new THREE.BoxGeometry(10, 10, 10, 10, 10, 10);
+const geometry = new THREE.BoxGeometry(10, 10, 10);
 
-const material = new THREE.MeshBasicMaterial({
-  color: 0xf11111,
-  wireframe: true,
+const material = new THREE.MeshStandardMaterial({
+  color: 0xf1234a,
 });
 
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 function animate() {
   requestAnimationFrame(animate);
 
   torus.rotation.x += 0.01;
   torus.rotation.z += 0.01;
+  torus.rotation.y = 0.01;
+
+  controls.update();
 
   renderer.render(scene, camera);
 }
